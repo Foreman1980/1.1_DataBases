e9649ffe04c681e7daebcfe97b841ad0d08b7e16
Foreman <sprain@yandex.ru>
2019-10-23 22:45:11 +0300

    Создание процедур и функций по материалам урока № 9 - 2 модуль видео № 3

--- 9 урок/BDShopFunction_les9.sql
+++ 9 урок/BDShopFunction_les9.sql
@@ -86,6 +86,90 @@ BEGIN
 END//
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS set_x;
+
+-- Процедура
+DROP PROCEDURE IF EXISTS declare_var;
+
+DELIMITER //
+CREATE PROCEDURE declare_var()
+BEGIN
+    DECLARE id, num int(11) DEFAULT 0;
+    DECLARE name, hello, temp TINYTEXT;
+END//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS declare_var;
+
+-- Процедура
+DROP PROCEDURE IF EXISTS declare_var;
+
+DELIMITER //
+CREATE PROCEDURE declare_var()
+BEGIN
+    DECLARE var TINYTEXT DEFAULT 'внешняя переменная';
+    BEGIN
+        DECLARE var TINYTEXT DEFAULT 'внутренняя переменная';
+        SELECT var;
+    END;
+    SELECT var;
+END//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS declare_var;
+
+-- Процедура
+DROP PROCEDURE IF EXISTS declare_var;
+
+DELIMITER //
+CREATE PROCEDURE declare_var()
+BEGIN
+    DECLARE var TINYTEXT DEFAULT 'внешняя переменная';
+    BEGIN
+        SELECT var;
+    END;
+    SELECT var;
+END//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS declare_var;
+
+-- Функция
+DROP FUNCTION IF EXISTS second_format;
+
+DELIMITER //
+CREATE FUNCTION second_format (seconds int)
+RETURNS varchar(255) DETERMINISTIC
+BEGIN
+    DECLARE days, hours, minutes int;
+
+    SET days = floor(seconds / 86400);
+    SET seconds = seconds - days * 86400;
+    SET hours = floor(seconds / 3600);
+    SET seconds = seconds - hours * 3600;
+    SET minutes = floor(seconds / 60);
+    SET seconds = seconds - minutes * 60;
+
+    RETURN concat(  days, ' days ',
+                    hours, ' hours ',
+                    minutes, ' minutes ',
+                    seconds, ' seconds ');
+END//
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS second_format;
+
+-- Процедура
+DROP PROCEDURE IF EXISTS numcatalogs;
+
+DELIMITER //
+CREATE PROCEDURE numcatalogs(OUT total int)
+BEGIN
+    SELECT count(*) INTO total FROM catalogs;
+END//
+DELIMITER ;
+
+
 
 
 
